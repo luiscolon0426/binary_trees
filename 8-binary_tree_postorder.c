315a400fac32aa07d9e4f8dc76006a90d81ad6c8
luiscolon0426 <luiscolon0426@gmail.com>
2022-03-22 04:33:18 +0000

    task 8, same as 7 but move the func

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - goes thru BT using postorder traversal
+ * @tree: pointer to the root node
+ * @func: function to call for each node
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+	if (tree)
+	{
+		binary_tree_postorder(tree->left, func);
+		binary_tree_postorder(tree->right, func);
+		func(tree->n);
+	}
+}
